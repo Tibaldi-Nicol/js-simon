bdf7df6e791766b07a51c12f77b2ff3bdf1ce3ca
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-03-28 09:42:43 +0100

    [UPD]Collego-dom

---
+++ simon_says_dom/js/script.js
@@ -0,0 +1,8 @@
+//definizio variabili e,delle funzioni e recupero elementi dom
+//recupero elementi dom che mi servono
+constform = document.getElementById('answerform');
+const inputField = document.querySelector('input');
+const countdown = document.getElementById('countdown');
+const numberList = document.getElementById('numbers-List');
+constmessage - document.getElementById('message');
+const instructions = document.getElementById('instructions');
