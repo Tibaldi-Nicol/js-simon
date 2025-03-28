95bb9d7f22fa9b85f6933f95cf63a4b3036be96d
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-03-28 10:06:47 +0100

    [UPD]Add-for-list

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -1,29 +1,45 @@
 //definizio variabili e,delle funzioni e recupero elementi dom
 //recupero elementi dom che mi servono
-constform = document.getElementById('answers-form');
+const form = document.getElementById('answers-form');
 const inputField = document.querySelector('input');
 const countdown = document.getElementById('countdown');
-const numberList = document.getElementById('numbers-List');
-constmessage - document.getElementById('message');
+const numberList = document.getElementById('numbers-list');
+const message = document.getElementById('message');
 const instructions = document.getElementById('instructions');
 const button = document.querySelector('button');
+
 //definisco variabili
 const min = 1;
 const max = 50;
 const totalNumbers = 5;
 const time = 10;
+let li = '';
+
 //definisco funzioni
 //funzione per generare numeri casuali
-const generateRandomNumber = (min,max,tot) => {
+const generateRandomNumber = (min, max, tot) => {
     //creo un array vuoto
     const numbers = [];
     //ciclo per generare numeri casuali
-    for(let i = 0; i < tot; i++) {
-       //genero un numero casuale 
-       const mum = Math.floor(Math.random() * (max - min + 1) + min);
-       //inserisco numero generato nellarray restituisco numero
-         numbers.push(num);
+    while (numbers.length < tot) {
+        //genero un numero casuale
+        const num = Math.floor(Math.random() * (max - min + 1) + min);
+        //verifico che il numero non sia già presente
+        if (!numbers.includes(num)) {
+            //inserisco numero generato nell'array
+            numbers.push(num);
+        }
     }
     return numbers;
+};
+
+//genero numeri casuali invocando la funzione
+const randomNumbers = generateRandomNumber(min, max, totalNumbers);
+
+//mostro numeri generati
+for (let i = 0; i < randomNumbers.length; i++) {
+    li += `<li>${randomNumbers[i]}</li>`;
 }
-console.log(generateRandomNumber(1,50,5));
+numberList.innerHTML = li;
+
+console.log(randomNumbers);
